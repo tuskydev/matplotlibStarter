015f91271ad40192253ae9d899073cfdcf9a428a
Mario Fernandez <diddleslip@gmail.com>
2023-10-30 07:46:10 -0400

    Adding starter file to Github

---
+++ matplotlibStarter.py
@@ -0,0 +1,32 @@
+# from matplotlib import pyplot as plt
+
+
+# agesX = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+# devY = [38496, 42000, 46752, 49320, 53200,
+#         56000, 62316, 64928, 67317, 68748, 73752]
+# plt.plot(agesX, devY, "k--", label="All Devs") ## X, and Y must be of same length
+
+
+# pyDevY = [45372, 48876, 53850, 57287, 63016,
+#             65998, 70003, 70000, 71496, 75370, 83640]
+# plt.plot(agesX, pyDevY, "b", label="Python")
+
+# plt.xlabel("Ages")
+# plt.ylabel("Median Salary (USD)")
+# plt.title("Median Salary (USD) by Age")
+
+
+# plt.legend()
+# plt.show()
+
+
+#  ---
+
+### .1X Code below:
+
+import pylab as plt
+
+
+plt.plot([1,2,3], color="r")
+
+plt.show()
